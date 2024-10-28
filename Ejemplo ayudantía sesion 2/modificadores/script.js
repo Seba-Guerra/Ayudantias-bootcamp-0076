61bb801652c80497d642374dfc3570658df7ba57
Sebastian Guerra <128656089+Seba-Guerra@users.noreply.github.com>
2024-10-28 18:43:21 -0300

    Update script.js
    FIX JS

--- Ejemplo ayudantía sesion 2/modificadores/script.js
+++ Ejemplo ayudantía sesion 2/modificadores/script.js
@@ -1,26 +1,26 @@
 function scrollToElement(elementSelector, instance = 0) {
-  // Select all elements that match the given selector
+  // se crea un buscador de elementos (ID) con el fin de instanciar un selector
   const elements = document.querySelectorAll(elementSelector);
-  // Check if there are elements matching the selector and if the requested instance exists
+  //  confirma si el elemento (ID) existe y de igual forma confirma que el instanciamiento suceda
   if (elements.length > instance) {
-    // Scroll to the specified instance of the element
-    elements[instance].scrollIntoView({ behavior: "smooth" });
+    // desplazamiento hacia el selector
+    elements[instance].scrollIntoView({ behavior: "smooth" }); // velocidad de desplazamiento
   }
 }
 
-const link1 = document.getElementById("link1");
+const link1 = document.getElementById("link1"); // constantes vinculantes con getElementByid
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
-link1.addEventListener("click", () => {
-  scrollToElement(".header");
+link1.addEventListener("click", () => { // Eventos listener 
+  scrollToElement(".header"); // Mueve el puntero hacia el primer header
 });
 
 link2.addEventListener("click", () => {
-  // Scroll to the second element with "header" class
+  // Mueve el puntero hacia el segundo header
   scrollToElement(".header", 1);
 });
 
 link3.addEventListener("click", () => {
   scrollToElement(".column");
-});
+}); // Mueve el puntero hacia el tercer puntero (Column)
